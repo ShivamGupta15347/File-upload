cf6ecf1563e0b3bbb04e0ffc8845b09bd9b48ea9
ShivamGupta15347 <shivamgupta15347@gmail.com>
2024-04-15 04:05:37 +0530

    reduced success

--- controllers/fileUlpoad.js
+++ controllers/fileUlpoad.js
@@ -26,8 +26,11 @@ function isFileTypeSupported(type, supportedTypes) {
   return supportedTypes.includes(type);
 }
 
-async function uploadFileToCloudinary(file, folder) {
+async function uploadFileToCloudinary(file, folder, quality) {
   const options = { folder };
+  if (quality) {
+    options.quality = quality;
+  }
   options.resource_type = "auto";
   return await cloudinary.uploader.upload(file.tempFilePath, options);
 }
@@ -62,7 +65,7 @@ exports.imageUpload = async (req, res) => {
     res.json({
       success: true,
       imageUrl: response.secure_url,
-      message: "Uploaded to cloud",
+      message: "Image Uploaded to cloud",
     });
   } catch (err) {
     console.error(err);
@@ -111,6 +114,47 @@ exports.videoUpload = async (req, res) => {
     res.status(400).json({
       success: false,
       message: "Error uploading",
+    });
+  }
+};
+
+exports.imageSizeReducer = async (req, res) => {
+  try {
+    const { name, tags, email } = req.body;
+    console.log(name, tags, email);
+
+    const file = req.files.imageFile;
+    console.log(file);
+
+    const supportedTypes = ["jpg", "jpeg", "png"];
+    const fileType = file.name.split(".")[1].toLowerCase();
+
+    if (!isFileTypeSupported(fileType, supportedTypes)) {
+      return res.status(400).json({
+        success: false,
+        message: "File Format Not supported",
+      });
+    }
+    const response = await uploadFileToCloudinary(file, "Codehelp", 30);
+    console.log(response);
+
+    const fileData = await File.create({
+      name,
+      tags,
+      email,
+      imageUrl: response.secure_url,
+    });
+
+    res.json({
+      success: true,
+      imageUrl: response.secure_url,
+      message: "Reduced Size image Uploaded to cloud",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({
+      success: false,
+      message: "Failed to reduce size",
     });
   }
 };
